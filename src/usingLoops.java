d3f37a530d326f5eb7f777a249417814236e5e45
Vahe <vaheant@gmail.com>
2019-12-06 00:01:17 +0400

    Added printF() function.

--- src/usingLoops.java
+++ src/usingLoops.java
@@ -20,6 +20,25 @@ public class usingLoops {
         }
     }
 
+    public static void printF(int len, char ch, char fillChar) {
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len; j++) {
+                if (i == len - 1 || j == len - 1 || i + j == len - 1) {
+                    System.out.print(ch);
+                } else {
+                    if (i + j < len - 1) {
+                        System.out.print(" ");
+                    } else {
+                        System.out.print(fillChar);
+                    }
+                }
+                if (j == len - 1) {
+                    System.out.println();
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
         int len = 8;
         char borderChar = '*';
@@ -27,5 +46,9 @@ public class usingLoops {
         printA(len, borderChar, fillChar);
         System.out.println("");
         printA(len, borderChar, borderChar);
+        System.out.println("");
+        printF(len, borderChar, fillChar);
+        System.out.println("");
+        printF(len, borderChar, borderChar);
     }
 }
