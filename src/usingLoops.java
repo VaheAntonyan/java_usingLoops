26dea9429f7c8030317663ebd01a6c0070589943
Vahe <vaheant@gmail.com>
2019-12-05 23:49:13 +0400

    Added printA() function.

--- src/usingLoops.java
+++ src/usingLoops.java
@@ -1,2 +1,31 @@
 public class usingLoops {
+
+
+    public static void printA(int len, char ch, char fillChar) {
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len; j++) {
+                if (i == 0 || j == 0 || i + j == len - 1) {
+                    System.out.print(ch);
+                } else {
+                    if (i + j > len - 1) {
+                        System.out.print(" ");
+                    } else {
+                        System.out.print(fillChar);
+                    }
+                }
+                if (j == len - 1) {
+                    System.out.println();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int len = 8;
+        char borderChar = '*';
+        char fillChar = ' ';
+        printA(len, borderChar, fillChar);
+        System.out.println("");
+        printA(len, borderChar, borderChar);
+    }
 }
