d498ef24b0c7c23dae046cdd6a255f3368c96ef7
Vahe <vaheant@gmail.com>
2019-12-06 12:36:44 +0400

    Added printG() function.

--- src/usingLoops.java
+++ src/usingLoops.java
@@ -115,11 +115,31 @@ public class usingLoops {
         }
     }
 
+    public static void printG(int height, char ch, char fillChar) {
+        for (int i = 0; i < height * 2 - 1; i++) {
+            for (int j = 0; j < height; j++) {
+                if ((i < height && j == height - 1 - i) || (i >= height && i == height - 1 + j) || j == height - 1) {
+                    System.out.print(ch);
+                } else {
+                    if ((i < height && j < height - 1 - i) || (i >= height && i > height - 1 + j)) {
+                        System.out.print(" ");
+                    } else {
+                        System.out.print(fillChar);
+                    }
+                }
+                if (j == height - 1) {
+                    System.out.println();
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
         int len = 8;
         char borderChar = '*';
         char fillChar = ' ';
         int height = 7;
+
         //printA()
         printA(len, borderChar, fillChar);
         System.out.println("");
@@ -154,6 +174,12 @@ public class usingLoops {
         printF(len, borderChar, fillChar);
         System.out.println("");
         printF(len, borderChar, borderChar);
+        System.out.println("");
+
+        //printG()
+        printG(height, borderChar, fillChar);
+        System.out.println("");
+        printG(height, borderChar, borderChar);
         System.out.println("");
     }
 }
